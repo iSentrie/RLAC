d42b3e49a0face69c35529751c208faa934bb4a1
Endpoints1337 <115175031+Endpoints1337@users.noreply.github.com>
2023-08-30 01:13:36 +0100

    8/30/23

--- Anti-Vpn/server.lua
+++ Anti-Vpn/server.lua
@@ -1,14 +1,13 @@
-local playerIP = GetPlayerEndpoint(source)
-local api = string.format("https://proxycheck.io/v2/%s?vpn=1&asn=1", playerIP)
-
 AddEventHandler('playerConnecting', function(playerName, kick, deferrals)
-    
-    PerformHttpRequest(api, function(response)
-        local api2 = json.decode(response or "")
+   local playerIP = GetPlayerEndpoint(source)
+   local api = string.format("https://proxycheck.io/v2/%s?vpn=1&asn=1", playerIP)
+   
+   PerformHttpRequest(api, function(response)
+       local api2 = json.decode(response or "")
 
-        if api2 and api2[playerIP] and api2[playerIP].proxy == "yes" then
-            kick("No VPNs allowed on this server.")
-            CancelEvent()
-        end
-    end)
+       if api2 and api2[playerIP] and api2[playerIP].proxy == "yes" then
+           kick("No VPNs allowed on this server.")
+           CancelEvent()
+       end
+   end)
 end)
