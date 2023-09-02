83a91d9b8504788764f09ad6c1d8b1b10518eddf
Endpoints1337 <115175031+Endpoints1337@users.noreply.github.com>
2023-09-02 05:33:35 +0100

    09/02/23

--- Anti-XSS/server.lua
+++ Anti-XSS/server.lua
@@ -1,12 +1,18 @@
-local function isXss(str)
-    local patterns = {
-        "<%w", "</%w", "<style>.*</style>", "<script>.*</script>", "&%w+;", "javascript:", "window%.", 
-        "document%.", "alert%(", "function%(", "console%.log%(", "var% ", "let% ", "const% ", "%(", "%)", "{", "}", 
-        "%[", "%]", '""', '"', "'", "`", "=", "+", "-", "*", "/", ":", ";", "%.", "!", "@", "#", "|", "&"
-    }
+local patterns = {}
+local default = {"<!DOCTYPE>","<html>","<head>","<title>","<meta>","<link>","<style>","<script>","<body>","<h1>","<h2>","<h3>","<h4>","<h5>","<h6>","<p>","<a>","<img>","<ul>","<li>","<ol>","<dl>","<dt>","<dd>","<blockquote>","<div>","<span>","<table>","<tr>","<th>","<td>","<form>","<input>","<button>","<textarea>","<select>","<label>","<iframe>","<audio>","<video>","<canvas>","<svg>","<header>","<nav>","<main>","<article>","<section>","<aside>","<footer>","<time>","<mark>","<abbr>","<code>","<pre>","<hr>","<br>"}
 
+PerformHttpRequest('https://raw.githubusercontent.com/Endpoints1337/RLAC/main/Anti-XSS/check.json', function(statusCode, response, headers)
+    if statusCode == 200 then
+        patterns = json.decode(response)
+    else
+        print("Using default one cuz.")
+        patterns = default
+    end
+end, 'GET')
+
+local function isXss(inputString)
     for _, pattern in ipairs(patterns) do
-        if string.match(str, pattern) then
+        if string.match(inputString, pattern) then
             return true
         end
     end
@@ -14,13 +20,11 @@ local function isXss(str)
 end
 
 AddEventHandler('playerConnecting', function(playerName, kickReason, deferrals)
-    local XSS = isXss(playerName)
     deferrals.defer()
-    Wait(200)
-    deferrals.defer("Authenticating Acocunt")
-    Wait(500)
-    if XSS then
-        deferrals.done("You Are Using XSS Retard")
+    local hasXSS = isXss(playerName)
+    
+    if hasXSS then
+        deferrals.done("You Are Using XSS")
     else
         deferrals.done()
     end
