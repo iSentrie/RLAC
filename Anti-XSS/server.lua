d8addacf3f295a412e648a9d3b385437cdbda9d1
Kmoc <91027110+kmocc@users.noreply.github.com>
2023-09-01 23:26:11 -0400

    9/1/23

--- Anti-XSS/server.lua
+++ Anti-XSS/server.lua
@@ -1,7 +1,4 @@
-local webhookURL = '' -- Discord webhook URL
-local kickMsg = 'Nice Try.'
-
-local function gayXSS(str)
+local function isXss(str)
     local patterns = {
         "<%w", "</%w", "<style>.*</style>", "<script>.*</script>", "&%w+;", "javascript:", "window%.", 
         "document%.", "alert%(", "function%(", "console%.log%(", "var% ", "let% ", "const% ", "%(", "%)", "{", "}", 
@@ -16,25 +13,15 @@ local function gayXSS(str)
     return false
 end
 
-AddEventHandler('playerConnecting', function(playerName, kick, deferrals)
-    if gayXSS(playerName) then
-        local identifiers = GetPlayerIdentifiers(source)
-        local steamID, discordID = nil, nil
-        
-        for _, id in ipairs(identifiers) do
-            if string.sub(id, 1, 6) == "steam:" then
-                steamID = id
-            elseif string.sub(id, 1, 9) == "discord:" then
-                discordID = id
-            end
-        end
-        
-        deferrals.done(kickMsg)
-        sendToDiscord(playerName, steamID, discordID)
+AddEventHandler('playerConnecting', function(playerName, kickReason, deferrals)
+    local XSS = isXss(playerName)
+    deferrals.defer()
+    Wait(200)
+    deferrals.defer("Authenticating Acocunt")
+    Wait(500)
+    if XSS then
+        deferrals.done("You Are Using XSS Retard")
+    else
+        deferrals.done()
     end
 end)
-
-function sendToDiscord(name, steam, discord)
-    local message = string.format("RLAC: %s\nSteam ID: %s\nDiscord ID: %s", name, steam or "N/A", discord or "N/A")
-    PerformHttpRequest(webhookURL, function() end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
-end
