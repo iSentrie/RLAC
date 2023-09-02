189a614dcf2c0e4a145b8af8fcd45a56e019001b
Kmoc <91027110+kmocc@users.noreply.github.com>
2023-09-01 23:39:03 -0400

    9/1/2023

--- Anti-VPN/server.lua
+++ Anti-VPN/server.lua
@@ -1,16 +1,13 @@
 local webhookUrl = Config.webhookUrl
--- Get the Player ip
-local api = string.format("https://proxycheck.io/v2/%s?vpn=1&asn=1", playerIP)
-local api2 = json.decode(response or "")
-local message = string.format("RLAC: Detected VPN, ip:", playerIP) 
 
-AddEventHandler('playerConnecting', function(playerName, kick, deferrals)   
+AddEventHandler('playerConnecting', function(playerName, setkick, deferrals)   
     local playerIP = GetPlayerEndpoint(source)
-    PerformHttpRequest(api, function(response)
-        if api2 and api2[playerIP] and api2[playerIP].proxy == "yes" then
-            kick("No VPNs allowed on this server.")
-            PerformHttpRequest(webhookURL, function() end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(("https://proxycheck.io/v2/%s?vpn=1&asn=1"):format(playerIP), function(response)
+        local dat = json.encode(response)
+        if response and dat[playerIP] and dat[playerIP].proxy == "yes" then
+            setkick("We Have Detected a VPN")
+            PerformHttpRequest(webhookUrl, function() end, 'POST', json.encode({content = ("Detected a VPN under the IP: %s"):format(playerIP)}), { ['Content-Type'] = 'application/json' })
             CancelEvent()
         end
     end)
- end)
+end)
